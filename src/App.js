a73c2157dc69f6b4e15f1bebccc0307bb0b4ad2f
italocunhas <italocunhasilva1@gmail.com>
2024-09-05 21:02:26 -0300

    com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
+import Nome from './components/Nome'
 
 function App() {
-  return (
+  const[aluno, setAluno] = useState('Sujeito Programador')
+
+  function handleChangeName(nome){
+    setAluno(nome);
+  }
+
+  return(
     <div>
       <h1>Bem vindo ao meu projeto</h1>
+      <h2>Olá: {aluno} </h2>
+      <button onClick={ () => handleChangeName('Lucas Oliveira') }>
+      Mudar
+      </button>
     </div>
   );
 }
 
 export default App;
+
+
